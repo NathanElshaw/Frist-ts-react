3f3c1a7f45eb6050eb48b17837c49d10afd75d5a
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-03-05 09:17:29 -0500

    added a counter

--- First-ts/src/App.tsx
+++ First-ts/src/App.tsx
@@ -1,18 +1,35 @@
-import { FC, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import FirstProp from "./Components/firstProp";
 
 function App() {
-  const [state, setState] = useState("Hello World");
+  const [state] = useState("Hello World");
 
+  let [count = 0, setCount] = useState(0);
+  function add() {
+    count++;
+    setCount(count);
+  }
+  function minus() {
+    count--;
+    setCount(count);
+  }
   return (
     <>
+      <p>{count}</p>
       <button
         onClick={() => {
-          setState("Good Night");
+          minus();
         }}
       >
-        Click
+        Minus
+      </button>
+      <button
+        onClick={() => {
+          add();
+        }}
+      >
+        Add
       </button>
       <FirstProp input={state} secondInput="and Again!" />
     </>
